23b6f993587a66e6e2aff1b87c56e2cd5d1dc5ea
ThomasM10 <56597384+ThomasM10@users.noreply.github.com>
2022-11-19 15:17:13 +0000

    Add files via upload

--- Backend/controllers/post.js
+++ Backend/controllers/post.js
@@ -7,6 +7,11 @@ var html = "";
 
 global.document = new JSDOM(html).window.document;
 
+// I guess this may be the area where the object sent from the frontend could go.  Example beneath.  
+var storedSendingObject = {}
+
+
+// This part, exports.createPost, is the part that executes reading from and writing to the excel sheets.
 exports.createPost = (req, res, _) => {
 
 const reader = require('xlsx')
@@ -20,27 +25,27 @@ const sheets = file.SheetNames
 for(let i = 0; i < sheets.length; i++)
 {
 const temp = reader.utils.sheet_to_json(
-        file.Sheets[file.SheetNames[i]])
+file.Sheets[file.SheetNames[i]])
 temp.forEach((res) => {
-    data.push(res)
+data.push(res)
 })
 }
 
 console.log(data)
 
-    let XLSX = require('xlsx')
+let XLSX = require('xlsx')
 
-    let excelFile = "../ReceivingExcelFile.xlsx";
-    let sheetName = "Sheet1"
-    let workbook = XLSX.readFile(excelFile);
-    let ws = workbook.Sheets[sheetName];
+let excelFile = "../ReceivingExcelFile.xlsx";
+let sheetName = "Sheet1"
+let workbook = XLSX.readFile(excelFile);
+let ws = workbook.Sheets[sheetName];
 
+// here it overwrites the worksheet.  Where it says data,
+// I instead need this to be the object with the name storedSendingObject
+workbook.Sheets[sheetName] = XLSX.utils.json_to_sheet(data);
+XLSX.writeFile(workbook, excelFile);
 
-    // Overwrite worksheet
-    workbook.Sheets[sheetName] = XLSX.utils.json_to_sheet(data);
-    XLSX.writeFile(workbook, excelFile);
-
-    // });
+// });
 
 };
 
@@ -58,7 +63,7 @@ console.log(data)
 // const server = http.createServer(buttonPage);
 
 // const server = http.createServer((req, res) => {
-      // res.end('This is my server response');
+// res.end('This is my server response');
 // })
 
 // const buttonPage = require('./01WorkingExcelEdit/Frontend/buttonPage.js')
